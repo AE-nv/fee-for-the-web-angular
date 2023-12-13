b319e4ca4c7b0f9c3921a794f927e66094ad1fc7
antonthomas <anton.thomas1310@gmail.com>
2023-12-13 13:23:04 +0100

    module setup

---
+++ src/app/my-pokemon/my-pokemon.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyPokemonRoutingModule } from './my-pokemon-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyPokemonRoutingModule
+  ]
+})
+export class MyPokemonModule { }
