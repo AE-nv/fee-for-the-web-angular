4c6e0a5f91463f8e03983559b93fa3514bd01ae2
antonthomas <anton.thomas1310@gmail.com>
2023-12-14 13:25:59 +0100

    add test

--- front-end/src/app/shared/services/message/message.service.spec.ts
+++ front-end/src/app/shared/services/message/message.service.spec.ts
@@ -3,19 +3,36 @@ import { TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('MessageService', () => {
   let service: MessageService;
+  let toastrServiceSpy: jasmine.SpyObj<ToastrService>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('ToastrService', ['error']);
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, ToastrModule.forRoot()],
-      providers: [ToastrService]
+      providers: [{ provide: ToastrService, useValue: spy }]
     });
     service = TestBed.inject(MessageService);
+    toastrServiceSpy = TestBed.inject(ToastrService) as jasmine.SpyObj<ToastrService>;
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call ToastrService.error with the correct arguments', () => {
+    // Arrange
+    const mockError = new HttpErrorResponse({ status: 500, statusText: 'Internal Server Error', error: 'Server Error' });
+
+    // Act
+    service.showApiError(mockError);
+
+    // Assert
+    expect(toastrServiceSpy.error).toHaveBeenCalledWith(mockError.message, 'API Error', { timeOut: 3000 });
   });
 });
