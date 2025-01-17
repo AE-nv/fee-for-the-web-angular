4b37c7bb09fff07cab30c916cdad23883bd75935
LexVDev <alexanderversweyveld@outlook.com>
2025-01-17 15:36:43 +0100

    2025

--- 2025/front-end/src/app/app.component.ts
+++ 2025/front-end/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AllPokemonComponent } from './components/all-pokemon/all-pokemon.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
--- 2025/front-end/src/app/components/all-pokemon/all-pokemon.component.ts
+++ 2025/front-end/src/app/components/all-pokemon/all-pokemon.component.ts
@@ -4,11 +4,11 @@ import { PokemonService } from '../../shared/services/pokemon.service';
 import { Observable } from 'rxjs';
 import { MessageService } from '../../shared/services/message/message.service';
 import { PokemonListComponent } from '../../shared/components/pokemon-list/pokemon-list.component';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'pokedex-all-pokemon',
-  imports: [PokemonListComponent, AsyncPipe],
+  imports: [PokemonListComponent, AsyncPipe, CommonModule],
   templateUrl: './all-pokemon.component.html',
   styleUrl: './all-pokemon.component.scss'
 })
