d20ae9910e4d6e30ef93dd341551031b03c17d36
LexVDev <alexanderversweyveld@outlook.com>
2025-01-17 16:12:47 +0100

    2025

--- 2025/front-end/src/app/components/all-pokemon/all-pokemon.component.spec.ts
+++ 2025/front-end/src/app/components/all-pokemon/all-pokemon.component.spec.ts
@@ -1,9 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AllPokemonComponent } from './all-pokemon.component';
-import { PokemonService } from '../../../shared/services/pokemon/pokemon.service';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { PokemonService } from '../../shared/services/pokemon.service';
 
 
 describe('AllPokemonComponent', () => {
@@ -12,17 +11,11 @@ describe('AllPokemonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-<<<<<<< HEAD:front-end/src/app/all-pokemon/components/all-pokemon/all-pokemon.component.spec.ts
-      imports: [HttpClientTestingModule, ToastrModule.forRoot()],
+      imports: [ToastrModule.forRoot()],
       providers: [PokemonService, ToastrService],
       declarations: [AllPokemonComponent]
     })
       .compileComponents();
-=======
-      imports: [AllPokemonComponent]
-    })
-    .compileComponents();
->>>>>>> calling-apis:2025/front-end/src/app/components/all-pokemon/all-pokemon.component.spec.ts
 
     fixture = TestBed.createComponent(AllPokemonComponent);
     component = fixture.componentInstance;
